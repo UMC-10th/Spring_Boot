326cc30bb2509697192c97c677ef15372e2f7e43
kannikii <kwnnh0124@dgu.ac.kr>
2026-04-12 20:00:34 +0900

    feat: khan week4 아키텍처 구조, Swagger 세팅

---
+++ umc10th/src/main/java/com/example/umc10th/domain/mission/exception/code/MissionSuccessCode.java
@@ -0,0 +1,7 @@
+package com.example.umc10th.domain.mission.exception.code;
+
+public enum MissionSuccessCode {
+    MISSION_CREATED,
+    MISSION_FETCHED
+}
+
---
+++ umc10th/src/main/java/com/example/umc10th/domain/review/exception/code/ReviewErrorCode.java
@@ -0,0 +1,7 @@
+package com.example.umc10th.domain.review.exception.code;
+
+public enum ReviewErrorCode {
+    REVIEW_NOT_FOUND,
+    INVALID_REVIEW_REQUEST
+}
+
