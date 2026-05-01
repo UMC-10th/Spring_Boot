aff5546012221b63412ce3969e2a289532bd7d85
issuejong <lsj63223@naver.com>
2026-05-02 02:22:55 +0900

    mission/#05: 리뷰 작성 DTO 생성

--- src/main/java/com/example/Spring_Boot/domain/review/dto/ReviewReqDTO.java
+++ src/main/java/com/example/Spring_Boot/domain/review/dto/ReviewReqDTO.java
@@ -1,4 +1,12 @@
 package com.example.Spring_Boot.domain.review.dto;
 
+import java.util.List;
+
 public class ReviewReqDTO {
+
+    public record CreateReviewRequest(
+            String content,
+            List<String> imageUrls,
+            Integer rating
+    ) {}
 }
