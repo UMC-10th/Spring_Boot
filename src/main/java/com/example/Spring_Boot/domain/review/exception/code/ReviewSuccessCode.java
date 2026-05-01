5cb6a722c34d759981d414daf4c6dc48f8c69fa9
issuejong <lsj63223@naver.com>
2026-05-02 02:27:05 +0900

    mission/#05: 리뷰 작성 성공 응답 코드 작성

--- src/main/java/com/example/Spring_Boot/domain/review/exception/code/ReviewSuccessCode.java
+++ src/main/java/com/example/Spring_Boot/domain/review/exception/code/ReviewSuccessCode.java
@@ -1,4 +1,21 @@
 package com.example.Spring_Boot.domain.review.exception.code;
 
-public enum ReviewSuccessCode {
+import com.example.Spring_Boot.global.apiPayload.code.BaseSuccessCode;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@RequiredArgsConstructor
+public enum ReviewSuccessCode implements BaseSuccessCode {
+
+    REVIEW_CREATED(
+            HttpStatus.OK,
+            "REVIEW200_1",
+            "리뷰 작성이 완료되었습니다."
+    );
+
+    private final HttpStatus status;
+    private final String code;
+    private final String message;
 }
