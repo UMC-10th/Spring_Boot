fb4e17108033babd0411fad4b0fbad43933d595b
issuejong <lsj63223@naver.com>
2026-05-02 12:55:06 +0900

    mission/#05: 미션 리스트 조회 성공 응답 코드 작성

--- src/main/java/com/example/Spring_Boot/domain/mission/exception/code/MissionSuccessCode.java
+++ src/main/java/com/example/Spring_Boot/domain/mission/exception/code/MissionSuccessCode.java
@@ -1,4 +1,21 @@
 package com.example.Spring_Boot.domain.mission.exception.code;
 
-public enum MissionSuccessCode {
+import com.example.Spring_Boot.global.apiPayload.code.BaseSuccessCode;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@RequiredArgsConstructor
+public enum MissionSuccessCode implements BaseSuccessCode {
+
+    USER_MISSION_LIST_OK(
+            HttpStatus.OK,
+            "USER_MISSION200_1",
+            "유저 미션 목록 조회에 성공했습니다."
+    );
+
+    private final HttpStatus status;
+    private final String code;
+    private final String message;
 }
