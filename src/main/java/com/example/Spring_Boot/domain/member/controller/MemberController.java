cfff0e743d8a7799ab8920c192b1a92129dcae91
issuejong <lsj63223@naver.com>
2026-05-02 13:44:07 +0900

    mission/#05: 회원가입 컨트롤러 추가

--- src/main/java/com/example/Spring_Boot/domain/member/controller/MemberController.java
+++ src/main/java/com/example/Spring_Boot/domain/member/controller/MemberController.java
@@ -1,4 +1,32 @@
 package com.example.Spring_Boot.domain.member.controller;
 
+import com.example.Spring_Boot.domain.member.dto.MemberReqDTO;
+import com.example.Spring_Boot.domain.member.dto.MemberResDTO;
+import com.example.Spring_Boot.domain.member.exception.code.MemberSuccessCode;
+import com.example.Spring_Boot.domain.member.service.MemberService;
+import com.example.Spring_Boot.global.apiPayload.ApiResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/members")
 public class MemberController {
+
+    private final MemberService memberService;
+
+    @PostMapping
+    public ApiResponse<MemberResDTO.CreateMemberResponse> createMember(
+            @RequestBody MemberReqDTO.CreateMemberRequest request
+            ) {
+        MemberResDTO.CreateMemberResponse response = memberService.createMember(request);
+
+        return ApiResponse.onSuccess(
+                MemberSuccessCode.MEMBER_CREATED,
+                response
+        );
+    }
 }
--- src/main/java/com/example/Spring_Boot/domain/member/service/MemberService.java
+++ src/main/java/com/example/Spring_Boot/domain/member/service/MemberService.java
@@ -1,4 +1,9 @@
 package com.example.Spring_Boot.domain.member.service;
 
+import com.example.Spring_Boot.domain.member.dto.MemberReqDTO;
+import com.example.Spring_Boot.domain.member.dto.MemberResDTO;
+
 public class MemberService {
+    public MemberResDTO.CreateMemberResponse createMember(MemberReqDTO.CreateMemberRequest request) {
+    }
 }
