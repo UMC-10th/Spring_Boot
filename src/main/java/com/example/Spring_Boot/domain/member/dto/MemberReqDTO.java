b08bfdbef849d0bc3e2021eb8a13415fc522def5
issuejong <lsj63223@naver.com>
2026-05-02 13:35:47 +0900

    mission/#05: 회원가입 DTO 생성

--- src/main/java/com/example/Spring_Boot/domain/member/dto/MemberReqDTO.java
+++ src/main/java/com/example/Spring_Boot/domain/member/dto/MemberReqDTO.java
@@ -1,4 +1,18 @@
 package com.example.Spring_Boot.domain.member.dto;
 
+import com.example.Spring_Boot.domain.member.enums.Gender;
+
+import java.time.LocalDate;
+import java.util.List;
+
 public class MemberReqDTO {
+
+    public record CreateMemberRequest(
+            String name,
+            Gender gender,
+            LocalDate birth,
+            String address,
+            List<Long> categoryIds
+    ) {
+    }
 }
--- src/main/java/com/example/Spring_Boot/domain/member/dto/MemberResDTO.java
+++ src/main/java/com/example/Spring_Boot/domain/member/dto/MemberResDTO.java
@@ -1,4 +1,19 @@
 package com.example.Spring_Boot.domain.member.dto;
 
+import com.example.Spring_Boot.domain.member.enums.Gender;
+
+import java.time.LocalDate;
+import java.util.List;
+
 public class MemberResDTO {
+
+    public record CreateMemberResponse(
+            Long userId,
+            String name,
+            Gender gender,
+            LocalDate birth,
+            String address,
+            List<Long> categoryIds
+    ) {
+    }
 }
