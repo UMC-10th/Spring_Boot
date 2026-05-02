251ff948ef8bb16417eef027404a5d2dca9f3fbc
issuejong <lsj63223@naver.com>
2026-05-02 13:06:37 +0900

    mission/#05: 미션 리스트 조회 컨트롤러 생성

--- src/main/java/com/example/Spring_Boot/domain/mission/controller/MissionController.java
+++ src/main/java/com/example/Spring_Boot/domain/mission/controller/MissionController.java
@@ -1,4 +1,35 @@
 package com.example.Spring_Boot.domain.mission.controller;
 
+import com.example.Spring_Boot.domain.mission.dto.MissionResDTO;
+import com.example.Spring_Boot.domain.mission.enums.Status;
+import com.example.Spring_Boot.domain.mission.exception.code.MissionSuccessCode;
+import com.example.Spring_Boot.domain.mission.service.MissionService;
+import com.example.Spring_Boot.global.apiPayload.ApiResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/api/missions")
 public class MissionController {
+
+    private final MissionService missionService;
+
+    // 미션 목록 조회
+    @GetMapping
+    public ApiResponse<MissionResDTO.MissionListResponse> getUserMissions(
+            @RequestParam Status status,
+            @RequestHeader("Authorization") String authorization
+    ) {
+        MissionResDTO.MissionListResponse response = missionService.getUserMissions(status, authorization);
+
+        return ApiResponse.onSuccess(
+                MissionSuccessCode.USER_MISSION_LIST_OK,
+                response
+        );
+    }
 }
--- src/main/java/com/example/Spring_Boot/domain/mission/service/MissionService.java
+++ src/main/java/com/example/Spring_Boot/domain/mission/service/MissionService.java
@@ -1,4 +1,9 @@
 package com.example.Spring_Boot.domain.mission.service;
 
+import com.example.Spring_Boot.domain.mission.dto.MissionResDTO;
+import com.example.Spring_Boot.domain.mission.enums.Status;
+
 public class MissionService {
+    public MissionResDTO.MissionListResponse getUserMissions(Status status, String authorization) {
+    }
 }
