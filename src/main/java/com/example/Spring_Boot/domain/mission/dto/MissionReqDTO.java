1774b127e58e109a8f073a28bf57564b0e062d47
hyedam <han9959e@naver.com>
2026-05-03 23:48:33 +0900

    mission/#5

--- src/main/java/com/example/Spring_Boot/domain/mission/dto/MissionReqDTO.java
+++ src/main/java/com/example/Spring_Boot/domain/mission/dto/MissionReqDTO.java
@@ -1,4 +1,13 @@
 package com.example.Spring_Boot.domain.mission.dto;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
 public class MissionReqDTO {
+
+    @Getter
+    @NoArgsConstructor
+    public static class UpdateStatusDTO {
+        private String status;
+    }
 }
--- src/main/java/com/example/Spring_Boot/domain/user/controller/UserController.java
+++ src/main/java/com/example/Spring_Boot/domain/user/controller/UserController.java
@@ -1,4 +1,21 @@
 package com.example.Spring_Boot.domain.user.controller;
 
+import com.example.Spring_Boot.domain.user.dto.UserReqDTO;
+import com.example.Spring_Boot.domain.user.dto.UserResDTO;
+import com.example.Spring_Boot.global.apiPayload.ApiResponse;
+import com.example.Spring_Boot.global.apiPayload.code.GeneralSuccessCode;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/auth")
 public class UserController {
+
+    @PostMapping("/users")
+    public ApiResponse<UserResDTO.JoinResultDTO> join(
+            @RequestBody UserReqDTO.JoinDTO request
+    ) {
+        return ApiResponse.onSuccess(GeneralSuccessCode.CREATED, null);
+    }
 }
