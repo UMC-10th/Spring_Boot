fce03db66e6987c82ea6051d27a066fa4e7a7c4e
OCJune <cjbormk11@gmail.com>
2026-05-10 22:09:30 +0900

    fix/#5-사용하지 않는 DTO 삭제

--- Juno/Week5/Week5_Mission/src/main/java/com/example/week5_mission/domain/mission/dto/MissionReqDTO.java
+++ Juno/Week5/Week5_Mission/src/main/java/com/example/week5_mission/domain/mission/dto/MissionReqDTO.java
@@ -1,13 +1,4 @@
 package com.example.week5_mission.domain.mission.dto;
 
-import lombok.Getter;
-
 public class MissionReqDTO {
-
-    @Getter
-    public static class Missions {
-        private String keyword;
-        private int cursor;
-        private int size;
-    }
 }
