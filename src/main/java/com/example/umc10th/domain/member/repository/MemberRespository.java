d719353a70aa2cf73dcc3578196605225a63158d
sekong11 <sekong1108@gmail>
2026-05-10 23:23:45 +0900

    mission/#06 : 엔티티 작성 및 매핑

--- src/main/java/com/example/umc10th/domain/member/repository/MemberRespository.java
+++ src/main/java/com/example/umc10th/domain/member/repository/MemberRespository.java
@@ -1,4 +1,10 @@
 package com.example.umc10th.domain.member.repository;
 
-public interface MemberRespository {
+import com.example.umc10th.domain.member.entity.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface MemberRespository extends JpaRepository<Member, Integer> {
+    Optional<Member> findByNameAndDeletedAtIsNull(String name);
 }
--- src/main/java/com/example/umc10th/domain/review/entity/Review.java
+++ src/main/java/com/example/umc10th/domain/review/entity/Review.java
@@ -1,4 +1,48 @@
 package com.example.umc10th.domain.review.entity;
 
-public class Review {
+import com.example.umc10th.domain.member.entity.Member;
+import com.example.umc10th.domain.mission.entity.Store;
+import com.example.umc10th.global.entity.BaseEntity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "review")
+public class Review extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "content", nullable = false, columnDefinition = "TEXT")
+    private String content;
+
+    @Column(name = "star", nullable = false)
+    private Float star;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "store_id")
+    private Store store;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "reply_id")
+    private Reply reply;
+
+    @OneToMany(mappedBy = "review")
+    private List<ReviewPhoto> reviewPhotoList = new ArrayList<>();
 }
+
